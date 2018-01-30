d72e6829dcfcb9e4de7349b9ab531c3cd489e4bb
Daniel Khoroshko <rasdaniil@gmail.com>
2018-01-30 19:12:28 +0200

    redirect on login/logout

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,21 +1,34 @@
-import { login } from '../api/usersApi.js';
-// import { push } from 'react-router-redux';
+import { login, logout } from '../api/usersApi.js';
+import { replace } from 'react-router-redux';
 
 export const loginUser = (log_email, log_password) => ({
   type: 'LOGIN_USER',
   log_email,
-  log_password
-})
-
-export const loginFail = (message) => ({
-  type: 'LOGIN_FAIL',
-  message
+  log_password,
 });
 
+export const loginFail = message => ({
+  type: 'LOGIN_FAIL',
+  message,
+});
 
-export const asyncLogin = (log_email, log_password) => (dispatch) => {
-  login(log_email, log_password).then( user => {
-      dispatch(loginUser(log_email, log_password));
-    })
-    // .catch(error => dispatch(registerFailure(error.message)));
+export const asyncLogin = (log_email, log_password) => async dispatch => {
+  try {
+    await login(log_email, log_password);
+    dispatch(loginUser(log_email, log_password));
+    dispatch(replace('/profile'));
+  }
+  catch(e) {
+    dispatch(registerFailure(error.message));
+  }
+};
+
+export const asyncLogout = () => async dispatch => {
+  try {
+    await logout();
+    dispatch(replace('/login'));
+  }
+  catch(e) {
+    // TODO: do something
+  }
 };
