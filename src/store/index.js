788f584be2108b0e1e4414aefd8e6f33927b2298
Daniel Khoroshko <rasdaniil@gmail.com>
2018-01-30 16:53:15 +0200

    code style

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,28 @@
-import {createStore, compose, applyMiddleware} from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { reactReduxFirebase } from 'react-redux-firebase';
-import logger from 'redux-logger'
-import * as firebase from 'firebase'
-import rootReducer from '../reducers';
+import logger from 'redux-logger';
+import * as firebase from 'firebase';
 import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
 
 // react-redux-firebase options
 const config = {
   userProfile: 'users', // firebase root where user profiles are stored
   enableLogging: true, // enable/disable Firebase's database logging
-}
-// Add redux Firebase to compose
-const createStoreWithFirebase = compose(
-  reactReduxFirebase(firebase, config)
-)(createStore)
+};
 
-let middlewares = [logger, thunk]
+// Add redux Firebase to compose
+const createStoreWithFirebase = compose(reactReduxFirebase(firebase, config))(
+  createStore,
+);
+
+let middleware = [logger, thunk];
 
 let store = createStoreWithFirebase(
   rootReducer,
   {},
   applyMiddleware(...middlewares),
-  window.devToolsExtension ? window.devToolsExtension() : f => f
+  window.devToolsExtension ? window.devToolsExtension() : f => f,
 );
 
 export default store;
