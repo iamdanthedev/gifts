4abeae58286a5c029a6a182bf58564d73eda34f0
Daniel Khoroshko <rasdaniil@gmail.com>
2018-01-30 18:40:00 +0200

    bug

--- src/middleware/core.js
+++ src/middleware/core.js
@@ -4,14 +4,16 @@ import { setLoaded } from '../actions/core';
 const coreMiddleware = ({ getState, dispatch }) => next => action => {
 
   // wait for react-redux-firebase to fetch profile
-  if (action.type === '@@reactReduxFirebase/SET_PROFILE') {
+  if (
+    action.type === '@@reactReduxFirebase/SET_PROFILE' ||
+    action.type === '@@reactReduxFirebase/AUTH_EMPTY_CHANGE'
+  ) {
     const result = next(action);
     dispatch(setLoaded());
     return result;
   }
 
   return next(action);
-
 };
 
 export default coreMiddleware;
