1dd57d3cc38d8354ead18a89070796fe8367fe87
Daniel Khoroshko <rasdaniil@gmail.com>
2018-01-30 18:03:27 +0200

    code style

--- src/components/loggedBubble/index.js
+++ src/components/loggedBubble/index.js
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
 class LoggedBubble extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-		// this.handlePass = this.handlePass.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+    // this.handlePass = this.handlePass.bind(this);
+  }
 
-	render() {
-		return (
+  render() {
+    return (
       <div className="login-bubble">
         <div className="username title-xs flex-property column-orient">
-          <i className="fa fa-user-circle-o" aria-hidden="true"></i>
+          <i className="fa fa-user-circle-o" aria-hidden="true" />
           Username
         </div>
       </div>
-    )
-	}
+    );
+  }
 }
 export default LoggedBubble;
