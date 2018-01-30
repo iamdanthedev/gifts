aadf2451ac610e4d07e21946554c4d955239cab0
Daniel Khoroshko <rasdaniil@gmail.com>
2018-01-30 19:00:54 +0200

    correct router reducer name

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,15 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux';
-import { firebaseReducer } from 'react-redux-firebase'
+import { firebaseReducer } from 'react-redux-firebase';
 //import thunk
-import registerUser from './users.js'
+import registerUser from './users.js';
+import coreReducer from './core';
 
 const EventsApp = combineReducers({
+  core: coreReducer,
   registerUser,
   firebase: firebaseReducer,
-  routing: routerReducer
-})
+  router: routerReducer
+});
 
 export default EventsApp
