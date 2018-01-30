25ff9b8bc4dba09a66b480e6ff9aadd8efa3102f
Daniel Khoroshko <rasdaniil@gmail.com>
2018-01-30 17:48:16 +0200

    added loader

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import P from 'prop-types';
 import Routes from './routes/routes.js'
 import LeftNav from './components/layout/leftNav';
+import Loader from './components/Loader';
 import './css/global.css';
 
 class App extends Component {
   render() {
+
     return (
       <div className="page">
-        <LeftNav />
-        <Routes />
+
+        {this.props.isLoading && (
+          <Loader />
+        )}
+
+        {!this.props.isLoading && (
+          <React.Fragment>
+            <LeftNav />
+            <Routes />
+          </React.Fragment>
+        )}
+
       </div>
     );
   }
 }
-export default App;
+
+App.propTypes = {
+  isLoading: P.bool
+};
+
+export default connect(s => ({
+  isLoading: s.core.isLoading
+}))(App);
