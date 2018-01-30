d8a2b5da6eab1a23db464a641655c08ea2728870
Daniel Khoroshko <rasdaniil@gmail.com>
2018-01-30 16:08:03 +0200

    + basic model

---
+++ src/model/user.js
@@ -0,0 +1,18 @@
+/**
+ * User Class
+ * @class
+ */
+export class User {
+
+  /**
+   * Instantiates a user object
+   * @param data
+   */
+  constructor(data) {
+    /**
+     * User email
+     * @type {string}
+     */
+    this.email = data.email;
+  }
+}
