adc95949653c28a6ca5e707540af591bab27d8fd
Daniel Khoroshko <rasdaniil@gmail.com>
2018-01-31 06:15:36 +0200

    sfc

--- src/components/friends/friendsBox/index.js
+++ src/components/friends/friendsBox/index.js
@@ -1,32 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import P from 'prop-types';
 import './style.css';
 
-class FriendBox extends Component{
-  state = {
-    status: this.props.status,
-    friendName: this.props.friendName,
-    friendEmail: this.props.friendEmail,
-    addToGroup: this.props.addToGroup,
-  }
-
-  handleAddToGroup = () => {
-    this.props.addToGroup(this.props.friendName, this.props.friendEmail);
-  }
-
-  render(){
-    return(
-      <div className="one-friend flex-property">
-        <div className="friend-status color-owes">
-        </div>
-        <div className="friend-info flex-property div-wrap">
-          <a className="friend-name">{this.props.friendName}</a>
-          <p className="friend-email"> Email: {this.props.friendEmail}</p>
-        </div>
-        {this.props.addToGroup && <div className="flex-center flex-property">
-          <a className="add-friend" onClick={this.handleAddToGroup}>+</a>
-        </div>}
+const FriendBox = ({ name, email, onAdd }) => (
+  <div className="one-friend flex-property">
+    <div className="friend-status color-owes" />
+    <div className="friend-info flex-property div-wrap">
+      <a className="friend-name">{name}</a>
+      <p className="friend-email"> Email: {email}</p>
+    </div>
+    {this.props.addToGroup && (
+      <div className="flex-center flex-property">
+        <a className="add-friend" onClick={onAdd}>
+          +
+        </a>
       </div>
-    )
-  }
-}
+    )}
+  </div>
+);
+
+FriendBox.propTypes = {
+  name: P.string.isRequired,
+  email: P.email.isRequired,
+  onAdd: P.func.isRequired,
+};
+
 export default FriendBox;
